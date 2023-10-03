3a9c6e7488ee22dfae1a3b2db98963233738ccbe
Gautam Anand <gautamgxtv@gmail.com>
2023-10-03 05:26:56 -0500

    Main file

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: disable-email-verification
+# about: A plugin to disable email verification in Discourse
+# version: 0.0.1
+# authors: Coder Gautam
+# url: https://github.com/codergautam/disable-email-verification-discourse
