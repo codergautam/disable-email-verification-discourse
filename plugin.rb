61d174c5696d99bdcf2f967f00bad66f9960acdc
Gautam Anand <gautamgxtv@gmail.com>
2023-10-03 19:39:31 -0500

    Fix issues with user preferences

--- plugin.rb
+++ plugin.rb
@@ -1,37 +1,34 @@
-# name: disable-email-verification
+# name: disable-email-verification-discourse
 # about: A plugin to disable email verification in Discourse
-# version: 0.0.1
+# version: 0.0.2
 # authors: Coder Gautam
 # url: https://github.com/codergautam/disable-email-verification-discourse
 
- # This code block disables email verification in Discourse
- # It does so by modifying the UsersController and User classes
- # Specifically, it modifies the user_params method in UsersController
- # and the create_email_token method in User
- # It only does so if SiteSetting.disable_email_verification is false
- after_initialize do
-   module AutoActivationPlugin
-     require_dependency 'users_controller'
-     require_dependency 'user'
+after_initialize do
+  module AutoActivationPlugin
+    require_dependency 'users_controller'
+    require_dependency 'user'
+    # if !SiteSetting.disable_email_verification
+    #   puts "Email verification is enabled"
+    # else
+    #   puts "Email verification is disabled"
+    # end
 
-     User.class_eval do
-       # This method creates an email token for the user
-       # It only does so if SiteSetting.disable_email_verification is false
-       def create_email_token
-         email_tokens.create(email: email) unless SiteSetting.disable_email_verification
-       end
-     end
+    User.class_eval do
+      def create_email_token
+        email_tokens.create(email: email) unless SiteSetting.disable_email_verification
+      end
+    end
 
-     UsersController.class_eval do
-       private
-       # This method modifies the user_params method in UsersController
-       # It sets the active field to true if SiteSetting.disable_email_verification is true
-       def user_params
-         merge_fields = {ip_address: request.ip}
-         merge_fields.merge!(active: true) if SiteSetting.disable_email_verification
-         params.permit!
-         params.merge(merge_fields)
-       end
-     end
-   end
- end
+    UsersController.class_eval do
+      private
+
+      def modify_user_params(attrs)
+        merge_fields = {ip_address: request.ip}
+        merge_fields.merge!(active: true) if SiteSetting.disable_email_verification
+        attrs.merge!(merge_fields)
+        attrs
+      end
+    end
+  end
+end
